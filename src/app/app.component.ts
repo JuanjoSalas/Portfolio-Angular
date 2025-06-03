1a59ca2065eeb1602c20c3246b0f730ca1dbe9a3
juan salas <juanjo.salas.dev@gmail.com>
2025-06-03 17:21:40 +0200

    footer end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, NavigationEnd, Event } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent,],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'portfolio-angular-app';
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((event: Event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        window.scrollTo({ top: 0, behavior: 'auto' });
+      });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,3 +16,4 @@ export const routes: Routes = [
     component: ProjectsComponent },
 
 ];
+
